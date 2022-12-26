d0e4f86bdc2bdd6893d830de5d97c0823bbeb186
Mark Nicholas Razon <84964385+marknicholasrazon@users.noreply.github.com>
2022-12-26 21:33:14 +0800

    Add files via upload

---
+++ greetings.bat
@@ -0,0 +1,16 @@
+@echo off
+title Matrix
+color 0a
+mode 1000
+goto Greeting
+
+:Greeting
+cls 
+echo Hello User!
+pause
+cls 
+goto Matrix
+
+:Matrix
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto Matrix
